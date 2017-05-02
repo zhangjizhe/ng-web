3ecfbd5ce9d514e665f7fcd7145f46271887cda6
liuwei <zhangjizhe@311.com>
2017-05-02 23:15:46 +0800

    5.2  jq不能用
    创建目录

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,22 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { SearchComponent } from './search/search.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { StarsComponent } from './stars/stars.component';
+import { ProductComponent } from './product/product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    SearchComponent,
+    CarouselComponent,
+    StarsComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
